521d1ed91272b75ed8be58003a110a482672a579
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-03-15 17:30:30 +0530

    Configuring Font

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import Head from "next/head";
+function Home(){
+  return (
+    <Fragment>
+    <Head>
+      <link href="https://fonts.googleapis.com/css2?family=Sono&display=swap" rel="stylesheet"></link>
+    </Head>
+    <h1>Home Page</h1>
+    </Fragment>
+  )
+}
+
+export default Home;
